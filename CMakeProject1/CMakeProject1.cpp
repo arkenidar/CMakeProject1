124a74c503ea51640b1e7a793b8b7281a7b774ed
Dario Cangialosi <dario.cangialosi@gmail.com>
2024-11-07 14:50:39 +0100

    Aggiungere i file di progetto.

---
+++ CMakeProject1/CMakeProject1.cpp
@@ -0,0 +1,15 @@
+﻿// CMakeProject1.cpp: definisce il punto di ingresso dell'applicazione.
+//
+
+#include "CMakeProject1.h"
+
+using namespace std;
+
+#include <fmt/core.h>
+
+int main()
+{
+	//cout << "Hello CMake." << endl;
+	fmt::print("Hello World! (from fmt::print) \n");
+	return 0;
+}
